a8802f638d8daee89fc487ceca3d068568810748
ghosind <ghosind@gmail.com>
2019-03-11 01:55:05 +0800

    add timer to clear expired items.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const sStore = Symbol('Store');
+const sClearTimer = Symbol('clearTimer');
+const sClearTime = Symbol('clearTime');
 
 class Check {
-  constructor() {
+  constructor(options) {
+    this[sClearTime] = options.clearTime || 60000;
     this[sStore] = [];
+    this[sClearTimer] = null;
   }
 
   checkAndSet(key, ttl) {
     const time = Date.now();
 
-    const item = this[sStore].findIndex(x => (x.key == key && x.expire > time));
+    const item = this[sStore].findIndex(x => (x.key === key && x.expire > time));
 
     if (item >= 0) {
       return false;
@@ -18,6 +22,17 @@ class Check {
       key,
       expire: time + ttl,
     });
+
+    if (!this[sClearTimer]) {
+      this[sClearTimer] = setTimeout(() => {
+        if (this[sStore].length > 0) {
+          this[sStore] = this[sStore].filter(x => x.expire > Date.now());
+        } else {
+          clearTimeout(this[sClearTime]);
+          this[sClearTime] = null;
+        }
+      }, this[sClearTime]);
+    }
 
     return true;
   }
