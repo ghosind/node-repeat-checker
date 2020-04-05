96d6fb241eff3f49544a8121221464b0213c3997
ghosind <ghosind@gmail.com>
2020-04-06 04:03:12 +0800

    Update memory checker.

--- src/memory.ts
+++ src/memory.ts
@@ -1,19 +1,33 @@
 // eslint-disable-next-line no-unused-vars
 import { BaseChecker, CheckerOption } from './base';
 
+export interface MemoryJob {
+  count: number;
+
+  clearCount: number;
+
+  expired: number;
+
+  maxCount: number;
+}
+
+interface CheckOptions {
+  maxCount?: number;
+}
+
 export class MemoryChecker extends BaseChecker {
-  private storage: any[];
+  private storage: Map<string, MemoryJob>;
 
   private timerId: NodeJS.Timeout | null;
 
   constructor(options?: CheckerOption) {
     super(options);
 
-    this.storage = [];
+    this.storage = new Map();
     this.timerId = null;
   }
 
-  checkAndSet(key: string, ttl: number, options?: any) {
+  checkAndSet(key: string, ttl: number, options?: CheckOptions) {
     if (!key || typeof key !== 'string' || key.length <= 0) {
       throw new TypeError('key was not string or key was empty');
     }
@@ -22,11 +36,10 @@ export class MemoryChecker extends BaseChecker {
       throw new TypeError('ttl was not number or ttl less than zero');
     }
 
-    const index = this.storage.findIndex((x: any) => x.key === key);
-    const item = index >= 0 ? this.storage[index] : null;
+    const item = this.storage.get(key);
 
     // item is exist and it is not expired
-    if (item && item.expire > Date.now()) {
+    if (item && item.expired > Date.now()) {
       if (item.maxCount > 0) {
         item.count += 1;
         return item.count < item.maxCount;
@@ -36,30 +49,29 @@ export class MemoryChecker extends BaseChecker {
     }
 
     if (!item) {
-      this.storage.push({
-        key,
-        expire: Date.now() + ttl,
+      this.storage.set(key, {
+        expired: Date.now() + ttl,
         count: 1,
         clearCount: 1,
-        maxCount: options && options.maxCount,
+        maxCount: options?.maxCount || 1,
       });
     } else {
-      item.expire = Date.now() + ttl;
+      item.expired = Date.now() + ttl;
       item.count = 1;
       item.clearCount += 1;
-      item.maxCount = options && options.maxCount;
+      item.maxCount = options?.maxCount || 1;
     }
 
     if (!this.timerId) {
       this.timerId = setInterval(() => {
-        if (this.storage.length > 0) {
-          // remove expired items.
-          this.storage = this.storage.filter(
-            (e: any) => (e.expire > Date.now() && e.clearCount > 0),
-          );
-
-          this.storage.forEach((e: any) => {
-            e.clearCount -= 1;
+        if (this.storage.size > 0) {
+          this.storage.forEach((job: MemoryJob, jobKey: string) => {
+            if (job.expired < Date.now() && job.clearCount <= 0) {
+              this.storage.delete(jobKey);
+            } else {
+              // eslint-disable-next-line no-param-reassign
+              job.clearCount -= 1;
+            }
           });
         } else if (this.timerId) {
           // stop timer when the items list was empty.
